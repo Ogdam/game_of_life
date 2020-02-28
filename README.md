3c6459509034f5b6557a37e4e9a2c607de027f25
Fabien <venier.fabien@outlook.Fr>
2020-02-28 12:36:15 +0100

    faire des truc " beau " avec bootstrap

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Todo List:  
   - [x] pouvoir modifier sans recharger la page
   - [ ] debuger les regles ><"
-  - [ ] modifier la taille du terrain en direct
+  - [x] modifier la taille du terrain en direct
 
 ![](game_of_life/images/inutile_sur_20.gif)
--- game_of_life/src/update_view.js
+++ game_of_life/src/update_view.js
@@ -1,9 +1,13 @@
+
+var Hauteur = 50;
+var Largeur = 50;
+
 ///////////////////////////////// CREATE EMPTY GAME /////////////////////////////////
 function create_empty_basic_view(){
     var base_view = "";
-    for (i=0;i<50;i++){
+    for (i=0;i<Hauteur;i++){
       base_view += "<tr>";
-      for (j=0;j<50;j++){
+      for (j=0;j<Largeur;j++){
         base_view += "<td class='case'></td>";
       }
       base_view += "</tr>";
@@ -48,9 +52,9 @@ function lets_play(matrice){
 
 function draw_new_board(matrice){
     var base_view = "";
-    for (i=0;i<50;i++){
+    for (i=0;i<Hauteur;i++){
       base_view += "<tr>";
-      for (j=0;j<50;j++){
+      for (j=0;j<Largeur;j++){
         if (matrice[i][j] == 0){
           base_view += "<td class='case'></td>";}
           else {base_view += "<td class='case alive'></td>";}
@@ -62,8 +66,15 @@ function draw_new_board(matrice){
 
     $('td.case').on("click", function(){
       $(this).toggleClass("alive");
-      console.log(1);
     });
+}
+
+///////////////////////////////// SET SIZE /////////////////////////////////
+
+function set_size(){
+  Hauteur = $('#hauteur').val();
+  Largeur = $('#largeur').val();
+  create_empty_basic_view();
 }
 
 ///////////////////////////////// CONTROL /////////////////////////////////
@@ -93,6 +104,11 @@ $( document ).ready(function() {
   $('#restart').on("click", function(){
     Play = false;
     create_empty_basic_view();
+  });
+
+  $('#size').on("click", function(){
+    Play = false;
+    set_size();
   });
 
 });
